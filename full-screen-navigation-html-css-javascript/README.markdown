657757d2653ca6d39a0dd8d310e209996675e9ae
Afarah92 <33521306+Afarah92@users.noreply.github.com>
2020-07-08 18:51:28 +0200

    Add files via upload

---
+++ full-screen-navigation-html-css-javascript/README.markdown
@@ -0,0 +1,5 @@
+# Full Screen Navigation | HTML, CSS & JavaScript
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Afarah92/pen/rNxpPvw](https://codepen.io/Afarah92/pen/rNxpPvw).
+
+
---
+++ full-screen-navigation-html-css-javascript/src/script.js
@@ -0,0 +1,20 @@
+function openNav () {
+  document.getElementById("open").style.display="none";
+  document.getElementById("nav").style.width="100%";
+
+
+  
+  
+  
+}
+
+
+function closeNav () {
+  document.getElementById("open").style.display ="block";
+  
+    document.getElementById("nav").style.width="0%";
+
+
+  
+  
+}
